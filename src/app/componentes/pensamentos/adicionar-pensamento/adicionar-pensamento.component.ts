b03ca0d24bea06ac14b555b2f5b1343fac607ee6
André Felipe <andre.felipe.2048@gmail.com>
2023-05-30 20:29:25 -0300

    feat: create component 'adicionar-pensamento'

---
+++ src/app/componentes/pensamentos/adicionar-pensamento/adicionar-pensamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-pensamento',
+  templateUrl: './adicionar-pensamento.component.html',
+  styleUrls: ['./adicionar-pensamento.component.css']
+})
+export class AdicionarPensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
