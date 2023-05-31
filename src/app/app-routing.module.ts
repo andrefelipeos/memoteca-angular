d987a3527023c238d034b02a5524aeb208ea14f8
André Felipe <andre.felipe.2048@gmail.com>
2023-05-31 20:10:22 -0300

    feat: configure routes to each component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdicionarPensamentoComponent } from './componentes/pensamentos/adicionar-pensamento/adicionar-pensamento.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mural-de-pensamentos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'adicionar-pensamento',
+    component: AdicionarPensamentoComponent
+  },
+  {
+    path: 'mural-de-pensamentos',
+    component: ListarPensamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
