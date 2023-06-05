83f23e7fdedf102585252cf426c856784520621b
André Felipe <andre.felipe.2048@gmail.com>
2023-06-05 12:55:33 -0300

    feat: add method 'listar' in 'pensamento.service'
    It gets a array of 'pensamentos' from the API.

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
+  private readonly API = 'http://localhost:3000/pensamentos'
+
   constructor(private httpClient: HttpClient) { }
+
+  listar(): Observable<Array<Pensamento>> {
+    return this.httpClient.get<Array<Pensamento>>(this.API)
+  }
 
 }
