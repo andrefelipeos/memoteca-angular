790c41359d1cc232400b0da4ceae109eb82dcb14
André Felipe <andre.felipe.2048@gmail.com>
2023-06-05 18:56:36 -0300

    feat: create component 'excluir-pensamento'

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  identificador: number = 0;
+
+  constructor(
+    private pensamentoService: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.identificador = parseInt(this.route.snapshot.paramMap.get('id')!)
+  }
+
+  excluirPensamento() {
+    this.pensamentoService.excluir(this.identificador).subscribe(() => {
+      this.router.navigate(['/mural-de-pensamentos'])
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/mural-de-pensamentos'])
+  }
+
+}
